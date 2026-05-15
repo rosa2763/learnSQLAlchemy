dc19a245ffaa3e350de9c7daf6f83641f5acf11a
rosa2763 <mrs.rosajacob@gmail.com>
2026-05-15 19:19:45 +0530

    May 15,2026

---
+++ 1_0create_db_table.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column,Integer,String,create_engine
+from sqlalchemy.orm import declarative_base
+
+db_url = "sqlite:///databse.db"
+engine = create_engine(db_url)
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer,primary_key=True)
+    name = Column(String)
+    Age = Column(Integer)
+
+
+
+Base.metadata.create_all(engine)
